b19b4901eea67e6f4468c63dbe3832a6381630e5
Kalkidan-Gashaw <162141690+Kalkidan-Gashaw@users.noreply.github.com>
2025-02-10 13:41:25 +0300

    Add files via upload

---
+++ vite.config.js
@@ -0,0 +1,20 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from "@tailwindcss/vite";
+
+// https://vite.dev/config/
+export default defineConfig({
+
+  theme:{
+    extend:{
+      colors: {
+        primary : "#2885ff",
+        secondary: "#EF863E"
+      },
+    },
+  },
+  plugins: [
+    react(),
+    tailwindcss()
+  ],
+});
